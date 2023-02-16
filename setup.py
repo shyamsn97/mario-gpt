94389b9ace79ab22f893f8140a4dc24a75e263ca
shyamsn97 <shyamsnair97@gmail.com>
2023-02-15 19:13:45 -0800

    bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="mario-gpt",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/kragniz/cookiecutter-pypackage-minimal",
     license='MIT',
 
