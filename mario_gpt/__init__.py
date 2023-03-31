c03d39ca54b524ab16ded40f0a07caca94a729a2
Miguel Piedrafita <github@miguel.build>
2023-03-31 11:15:45 +0100

    wip

--- mario_gpt/__init__.py
+++ mario_gpt/__init__.py
@@ -1,12 +1,11 @@
 from mario_gpt.dataset import MarioDataset
-from mario_gpt.lm import MarioBert, MarioGPT, MarioLM
+from mario_gpt.lm import MarioGPT, MarioLM
 from mario_gpt.prompter import Prompter
 from mario_gpt.sampler import GPTSampler, SampleOutput
 
 __all__ = [
     "Prompter",
     "MarioDataset",
-    "MarioBert",
     "MarioGPT",
     "MarioLM",
     "SampleOutput",
